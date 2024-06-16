f7db1154aef864da68be3d1e905e9b063fd24074
Taylor Monacelli <taylormonacelli@gmail.com>
2024-06-15 22:59:17 -0700

    fixed

--- main.tf
+++ main.tf
@@ -1,24 +1,36 @@
 provider "aws" {
- region = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_dynamodb_table" "example_table" {
- name           = "heavybee-table"
- billing_mode   = "PAY_PER_REQUEST"
- hash_key       = "fromEmail"
+resource "aws_dynamodb_table" "heavybee_table" {
+  name         = "heavybee-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "fromEmail"
 
- attribute {
-   name = "fromEmail"
-   type = "S"
- }
+  attribute {
+    name = "fromEmail"
+    type = "S"
+  }
 
- attribute {
-   name = "fromName"
-   type = "S"
- }
+  attribute {
+    name = "fromName"
+    type = "S"
+  }
 
- attribute {
-   name = "fromDomain"
-   type = "S"
- }
+  attribute {
+    name = "fromDomain"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "fromName-index"
+    hash_key        = "fromName"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "fromDomain-index"
+    hash_key        = "fromDomain"
+    projection_type = "ALL"
+  }
 }
